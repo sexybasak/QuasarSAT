b12412e3abb4f765a4f9f724e4bbd0622475feae
sexybasak <Basakreshov@gmail.com>
2026-06-09 21:51:51 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' // Ensure App.jsx has a capital A
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
